ebb0c0e41a80c33ec60ae97a32e6068f6d0de68e
Zach / Virus <thevirusofdoom1337@gmail.com>
2025-06-07 21:47:48 -0400

    frontend basics

---
+++ BMI.xaml.cs
@@ -0,0 +1,14 @@
+namespace BMI_Calculator;
+
+public partial class BMI : ContentPage
+{
+	public BMI()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
